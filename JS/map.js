20b925d066e271f3286faf784dbabe5f27bd82e3
Eleanor Roberts <eleanorlroberts@gmail.com>
2021-12-14 11:25:32 +0000

    added functionality to add a new route

--- JS/map.js
+++ JS/map.js
@@ -11,20 +11,30 @@ function myMap() {
 
     map.addListener("click", (mapsMouseEvent) => {
         infoWindow.close()
+        let infoPosition = mapsMouseEvent.latLng
         infoWindow = new google.maps.InfoWindow({
             content: generateForm(),
-            position: mapsMouseEvent.latLng,
+            position: infoPosition
         });
 
         infoWindow.open(map)
+
+        google.maps.event.addListener(infoWindow, 'domready', () => {
+            document.querySelector('#formWindow').addEventListener('submit', (e) => {
+                e.preventDefault()
+                handleSubmit(infoPosition)
+            })
+        });
     })
+
 }
 
 function generateForm() {
-    let formContent = '<form class="d-flex flex-column">'
-    formContent += '<label for="name"></label><input type="text" id="name" name="name" minlength="5" maxlength="20" placeholder="Name of walk..." />'
+    let formContent = '<form class="d-flex flex-column" id="formWindow">'
+    formContent += '<label for="name"></label>'
+    formContent += '<input type="text" id="name" name="name" minlength="0" maxlength="20" placeholder="Name of walk..." />'
     formContent += '<label class="mt-2">Length of Walk (mins)</label>'
-    formContent += '<input type="number" id="lengthOfWalk" name="lengthOfWalk" min="0" max="500" />'
+    formContent += '<input type="number" id="length" name="length" min="0" max="500" />'
     formContent += '<label class="mt-2">Difficulty</label>'
     formContent += '<input type="number" id="difficulty" name="difficulty" min="1" max="5" />'
     formContent += '<label class="mt-2">Start Instructions</label>'
@@ -32,4 +42,20 @@ function generateForm() {
     formContent += '<input class="my-2" type="submit" value="Submit" />'
     formContent += '</form>'
     return formContent;
+}
+
+async function handleSubmit(position) {
+    let newRoute = {
+        name: document.querySelector('#name').value,
+        length: document.querySelector('#length').value,
+        difficulty: document.querySelector('#difficulty').value,
+        startInstructions: document.querySelector('#startInstructions').value,
+        markersArray: [position]
+    }
+
+    await fetch('http://localhost:3000', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(newRoute)
+    })
 }
