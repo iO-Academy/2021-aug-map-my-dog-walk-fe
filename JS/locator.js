0a061a4621a860efd49ea60b8850130d8f53d12c
Jake Rooms <jakeprooms@outlook.com>
2021-12-16 16:25:20 +0000

    fixed locator bug

--- JS/locator.js
+++ JS/locator.js
@@ -13,8 +13,8 @@ function handleLocatorClick(e){
 }
 
 function showPosition(position) {
-    locationDiv.innerHTML = "<div>Latitude: " + position.coords.latitude + '</div>'
-        "<div>Longitude: " + position.coords.longitude + '</div>';
+    locationDiv.innerHTML = '<div>Latitude: ' + position.coords.latitude + '</div>' +
+        '<div>Longitude: ' + position.coords.longitude + '</div>';
 }
 
 function errorCallback(){
